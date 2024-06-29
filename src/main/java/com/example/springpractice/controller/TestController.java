09157f25bcd6e0c43e998b14a6d3bcadd4c8571a
yoonjae <backendINFJ@github.com>
2024-06-30 04:33:56 +0900

    Feat : project test

--- src/main/java/com/example/springpractice/controller/TestController.java
+++ src/main/java/com/example/springpractice/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.example.springpractice.controller;
 
 import com.example.springpractice.service.TestService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/example/springpractice/service/TestService.java
+++ src/main/java/com/example/springpractice/service/TestService.java
@@ -8,5 +8,4 @@ public class TestService {
     public String test() {
         return "test";
     }
-
 }
